ac4a1216491a56897da47c4cff83cf105384ad97
H2 <hemoptysisheart@gmail.com>
2022-08-28 12:25:35 +0900

    🚧 Case #3 추가.main

--- app/src/main/java/com/github/hemoptysisheart/ditestapplication/app/activity/Case3Activity.kt
+++ app/src/main/java/com/github/hemoptysisheart/ditestapplication/app/activity/Case3Activity.kt
@@ -1,18 +1,17 @@
 package com.github.hemoptysisheart.ditestapplication.app.activity
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,14 +20,14 @@ import com.github.hemoptysisheart.ditestapplication.app.ui.theme.DiTestApplicati
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class LauncherActivity : ComponentActivity() {
+class Case3Activity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DiTestApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    LauncherCompose()
+                    Case3Layout()
                 }
             }
         }
@@ -36,31 +35,18 @@ class LauncherActivity : ComponentActivity() {
 }
 
 @Composable
-fun LauncherCompose() {
-    val context = LocalContext.current
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Button(onClick = { context.startActivity(Intent(context, Case1Activity::class.java)) }) {
-            Text(text = stringResource(R.string.activity_case1_title))
-        }
-        Spacer(modifier = Modifier.height(24.dp))
-        Button(onClick = { context.startActivity(Intent(context, Case2Activity::class.java)) }) {
-            Text(text = stringResource(R.string.activity_case2_title))
-        }
-        Spacer(modifier = Modifier.height(24.dp))
-        Button(onClick = { context.startActivity(Intent(context, Case3Activity::class.java)) }) {
-            Text(text = stringResource(R.string.activity_case3_title))
-        }
+fun Case3Layout() {
+    Column(Modifier.fillMaxSize()) {
+        Text(text = stringResource(R.string.activity_case3_title))
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(text = stringResource(R.string.activity_case3_description))
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun LauncherComposePreview() {
+fun Case3LayoutPreview() {
     DiTestApplicationTheme {
-        LauncherCompose()
+        Case3Layout()
     }
 }
